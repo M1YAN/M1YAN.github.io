c62e4769da7e7cae551db5545606dfadf6a39660
M1YAN <2869845413@qq.com>
2024-03-09 09:00:05 +0800

    Site updated: 2024-03-09 09:00:05

--- js/main.js
+++ js/main.js
@@ -122,6 +122,7 @@
 
     const $toc = $('#toc');
     if ($toc.length > 0) {
+        $toc.addClass('column-left is-sticky');//add
         const $mask = $('<div>');
         $mask.attr('id', 'toc-mask');
 
